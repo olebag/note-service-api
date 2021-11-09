6b2d4dd955f07c57f0bb3363c1e9e325c47f7066
Shamistan <shemistan1@gmail.com>
2021-11-09 23:23:53 +0300

    finished lesson_3 v2

--- internal/app/api/note.go
+++ internal/app/api/note.go
@@ -2,42 +2,18 @@ package api
 
 import (
 	"fmt"
-	"os"
 )
 
-//Для своей предметной области описать структуры и
-//реализовать методы работы с ними к примеру String()
-//type User struct {
-//	UserId uint64
-//	// ...
-//}
-
-func OpenCloseFile(file string) {
-	for i := 0; i < 5; i++ {
-		data, err := os.Open(file)
-		if err != nil {
-			fmt.Println("Error", err)
-			return
-		}
-
-		fmt.Println(data.Name())
-		defer func(data *os.File) {
-			err := data.Close()
-			if err != nil {
-				fmt.Println("Error", err)
-			}
-		}(data)
-	}
-
-}
-
+// User
 type User struct {
-	id          uint64
+	Id          uint64
 	UserId      uint32
 	ClassroomId uint32
 	DocumentId  uint32
 }
 
-func (a *User) String() {
-	fmt.Println(a.id, a.UserId, a.ClassroomId, a.DocumentId)
+func (u *User) String() {
+	fmt.Printf(
+		" Id = %v\n UserId = %v\n ClasssroomId = %v\n DocumentId = %v",
+		u.Id, u.UserId, u.ClassroomId, u.DocumentId)
 }
