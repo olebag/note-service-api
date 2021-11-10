c36479be710bd7dc1aaff86cf5deea66d893409e
Shamistan <shemistan1@gmail.com>
2021-11-10 20:08:35 +0300

    finished lesson_3 v6

--- cmd/note-service-api/main.go
+++ cmd/note-service-api/main.go
@@ -12,23 +12,22 @@ func main() {
 	val1, err1 := utills.FilterSlice([]string{"d", "r", "t", "b", "a"})
 	if err1 != nil {
 		fmt.Printf("error to start function FilterSlice %s", err1)
-	} else {
-		fmt.Println(val1)
+		return
 	}
+	fmt.Println(val1)
 
 	val2, err2 := utills.SwapKeyAndValue(map[int32]string{1: "one"})
 	if err2 != nil {
 		fmt.Printf("error to start function SwapKeyAndValue %s", err2)
-	} else {
-		fmt.Println(val2)
+		return
 	}
+	fmt.Println(val2)
 
 	err3 := OpenCloseFile("cmd/note-service-api/text.txt")
 	if err3 != nil {
 		fmt.Printf("OpenCloseFile() function execution error %s\n\n", err3)
-	} else {
-		fmt.Printf("OpenCloseFile() function comleted\n\n")
 	}
+	fmt.Printf("OpenCloseFile() function comleted\n\n")
 
 	data := []api.User{
 		{Id: 1, UserId: 1, ClassroomId: 23, DocumentId: 6},
@@ -42,16 +41,14 @@ func main() {
 	val4, err4 := utills.ConvertSliceToMap(data)
 	if err4 != nil {
 		fmt.Printf("error to start function ConvertSliceToMap %s", err4)
-	} else {
-		fmt.Println(val4)
 	}
+	fmt.Println(val4)
 
 	val5, err5 := utills.SplitSlice(data, 3)
 	if err5 != nil {
 		fmt.Printf("error to start function SplitSlice %s", err5)
-	} else {
-		fmt.Println(val5)
 	}
+	fmt.Println(val5)
 
 	for _, v := range data {
 		v.String()
